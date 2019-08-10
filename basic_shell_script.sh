500aa0b352b68ed6d574be7f5aa56b7afff384d1
devikajaiswal <53897810+devikajaiswal@users.noreply.github.com>
2019-08-10 17:11:59 +0530

    Create basic_shell_script.sh

---
+++ basic_shell_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mail_body="GL reports have been triggered for : `date +%d/%m/%Y`"
+echo $mail_body | mail -s "GL REPORT STATUS" devika.jaiswal@kpn.com
+
+x=1
+
+prev_month=$(echo $x | /usr/xpg4/bin/awk -v date="$(date "+%d %m %Y")" -v OFS="_" '
+{
+  num=$0
+  split(date, a); d=a[1]; m=a[2]; y=a[3]
+  m-=num
+  while(m < 1) {m+=12; y--}
+  printf("%02d%s%s\n", m)
+}')
+
+dd=`date +%d`
+yr=`date +%Y`
+
+first_arg=$prev_month-$dd-$yr
+echo $first_arg
+
+second_arg=`date +%m-%d-%Y`
+echo $second_arg
